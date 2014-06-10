e58fd3b25d63ce2e8f7c0378e6359e650fc5de88
theHarvester <james.harvey994@gmail.com>
2014-06-10 20:15:41 +1000

    Added edit feature

--- app/views/blog/article.blade.php
+++ app/views/blog/article.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <h1 class="article-title section">{{ $post->title }}</h1>
 <div class="article-body section">
+@if(Auth::check())
+    {{ HTML::link('article/'.$post->id, 'Edit', null, false) }}<br />
+@endif
 {{ $post->article }}
 </div>
 @stop
